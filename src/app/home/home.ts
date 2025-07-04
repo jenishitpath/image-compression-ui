9b5425a8d50c2aa29125efc61a280a5726d23ea8
jenishitpath <jenishp@itpathsolutions.com>
2025-07-04 14:53:00 +0530

    Ui enhancment

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -17,25 +17,54 @@ export class Home {
   width = 0;
   height = 0;
   zip = false;
+  quality = 75;
+  format = 'jpg';
+  grayscale = false;
+  rotate = 0;
+  flip = '';
+  crop = false;
+  bgColor = 'white';
+  namePrefix = 'img';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   onFileChange(event: any) {
     this.files = Array.from(event.target.files);
   }
 
   uploadImages() {
+    if (this.files.length === 0) {
+      alert("Please select at least one image.");
+      return;
+    }
+
     const formData = new FormData();
     this.files.forEach(file => formData.append('files', file));
 
-    const queryParams = `?width=${this.width}&height=${this.height}&zip=${this.zip}`;
-    this.http.post(`https://localhost:7030/api/image/upload${queryParams}`, formData, { responseType: 'blob' })
-      .subscribe(blob => {
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = this.zip ? 'images.zip' : 'image.png';
-        a.click();
-      });
+    const params = new URLSearchParams({
+      width: this.width.toString(),
+      height: this.height.toString(),
+      zip: this.zip.toString(),
+      quality: this.quality.toString(),
+      format: this.format,
+      grayscale: this.grayscale.toString(),
+      rotate: this.rotate.toString(),
+      flip: this.flip,
+      crop: this.crop.toString(),
+      bgColor: this.bgColor,
+      namePrefix: this.namePrefix
+    });
+
+    this.http.post(`https://localhost:7030/api/image/upload?${params.toString()}`, formData, {
+      responseType: 'blob'
+    }).subscribe(blob => {
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = this.zip ? 'images.zip' : `image.${this.format}`;
+      a.click();
+    }, err => {
+      alert("Upload failed.");
+    });
   }
 }
